94279291446333e5a6c66d36152ea532a004f45a
Pedro de Jesus <pedro@digirati.com.br>
2025-10-09 18:23:11 -0300

    ascii art

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
 function Home() {
-  return <h1>Hello World</h1>
+  const asciiArt = String.raw`
+ _____________________________
+(   Nós vamos ficar ricos!!!   )
+ -----------------------------
+      o    ^__^
+        o  (oo)\_______
+          (__)\        )\/\
+              ||----w||
+              ||     ||
+`;
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <pre
+        style={{
+          fontFamily: "monospace",
+          fontSize: "16px",
+          whiteSpace: "pre",
+          display: "inline-block",
+          textAlign: "left",
+        }}
+      >
+        {asciiArt}
+      </pre>
+    </div>
+  );
 }
 
 export default Home;

1ac51210c6b9ee259afb946c32b9b64f0f3ecd53
Pedro de Jesus <pedro.furtado@stone.com.br>
2026-02-25 20:38:23 -0300

    fix: add missing `orchestrator` changes

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -10,6 +10,7 @@ const emailHttpUrl = `http://${process.env.EMAIL_HTTP_HOST}:${process.env.EMAIL_
 
 async function waitForAllServices() {
   await waitForWebServer();
+  await waitForEmailServer();
 
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
@@ -19,6 +20,21 @@ async function waitForAllServices() {
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
+
+      if (response.status !== 200) {
+        throw Error();
+      }
+    }
+  }
+
+  async function waitForEmailServer() {
+    return retry(fetchEmailPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+
+    async function fetchEmailPage() {
+      const response = await fetch(emailHttpUrl);
 
       if (response.status !== 200) {
         throw Error();

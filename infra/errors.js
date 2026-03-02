f72c277c569a25ddfe4502abd790d53e699f1841
Pedro de Jesus <pedro.furtado@stone.com.br>
2026-03-02 19:03:38 -0300

    refactor(email): enhance error loggin

--- infra/errors.js
+++ infra/errors.js
@@ -19,12 +19,13 @@ export class InternalServerError extends Error {
 }
 
 export class ServiceError extends Error {
-  constructor({ cause, message }) {
+  constructor({ cause, message, action, context }) {
     super(message || "Serviço indisponível no momento.", {
       cause,
     });
     this.name = "ServiceError";
-    this.action = "Verifique se o serviço está disponível.";
+    this.action = action || "Verifique se o serviço está disponível.";
+    this.context = context;
     this.statusCode = 503;
   }
 
@@ -33,6 +34,7 @@ export class ServiceError extends Error {
       name: this.name,
       message: this.message,
       action: this.action,
+      context: this.context,
       status_code: this.statusCode,
     };
   }
